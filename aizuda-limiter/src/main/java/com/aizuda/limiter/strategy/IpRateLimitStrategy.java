2701fdca9e5e6fca72cfdb3762df0b865e7b8a92
VampireAchao <achao1441470436@gmail.com>
2021-11-20 14:03:30 +0800

    优化上次的提交

--- aizuda-limiter/src/main/java/com/aizuda/limiter/strategy/IpRateLimitStrategy.java
+++ aizuda-limiter/src/main/java/com/aizuda/limiter/strategy/IpRateLimitStrategy.java
@@ -20,24 +20,31 @@ import java.util.stream.Stream;
  */
 public class IpRateLimitStrategy implements IRateLimitStrategy {
 
+    public static final String UNKNOWN = "unknown";
+    public static final String[] HEADERS_FOR_TRY = {"x-forwarded-for", "Proxy-Client-IP", "WL-Proxy-Client-IP"};
+    public static final Integer LEGAL_HEADER_LENGTH = 15;
+
     @Override
     public String getType() {
-        return IP;
+        return "ip";
     }
 
     @Override
     public String getKey() {
         HttpServletRequest request = this.getRequest();
-        final AtomicReference<String> ip = new AtomicReference<>(request.getRemoteAddr());
+        final AtomicReference<String> ip = new AtomicReference<>();
         // 对配置好的用于尝试的header进行尝试
         Stream.of(HEADERS_FOR_TRY).forEach((String header) -> {
             if (headerNotMatch(ip)) {
                 ip.set(request.getHeader(header));
             }
         });
+        if (headerNotMatch(ip)) {
+            ip.set(request.getRemoteAddr());
+        }
         // 最后对获取到的ip进行处理
-        Optional.of(ip).map(AtomicReference::get).filter(header -> header.length() > LEGAL_HEADER_LENGTH).filter(header -> header.indexOf(COMMA) > ZERO)
-                .map(header -> header.substring(ZERO, header.indexOf(COMMA))).ifPresent(ip::set);
+        Optional.of(ip).map(AtomicReference::get).filter(header -> header.length() > LEGAL_HEADER_LENGTH).filter(header -> header.indexOf(",") > 0)
+                .map(header -> header.substring(0, header.indexOf(","))).ifPresent(ip::set);
         return ip.get();
     }
 
@@ -48,6 +55,6 @@ public class IpRateLimitStrategy implements IRateLimitStrategy {
      * @return 不满足条件，返回true
      */
     private boolean headerNotMatch(AtomicReference<String> header) {
-        return !Optional.of(header).map(AtomicReference::get).filter(ip -> ip.length() != ZERO).filter(ip -> !UNKNOWN.equalsIgnoreCase(ip)).isPresent();
+        return !Optional.of(header).map(AtomicReference::get).filter(ip -> ip.length() != 0).filter(ip -> !UNKNOWN.equalsIgnoreCase(ip)).isPresent();
     }
 }
