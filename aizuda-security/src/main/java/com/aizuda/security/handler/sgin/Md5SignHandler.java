ee0cc41c631a2e85b18beace223865aaf297d00f
huang <649875413@qq.com>
2021-12-02 01:21:22 +0800

    #update

--- aizuda-security/src/main/java/com/aizuda/security/handler/sgin/Md5SignHandler.java
+++ aizuda-security/src/main/java/com/aizuda/security/handler/sgin/Md5SignHandler.java
@@ -1,3 +1,8 @@
+/*
+ * 爱组搭 http://aizuda.com 低代码组件化开发平台
+ * ------------------------------------------
+ * 受知识产权保护，请勿删除版权申明
+ */
 package com.aizuda.security.handler.sgin;
 
 import org.springframework.util.DigestUtils;
@@ -8,7 +13,7 @@ import org.springframework.util.DigestUtils;
  * 尊重知识产权，CV 请保留版权，爱组搭 http://aizuda.com 出品
  *
  * @author imantou
- * @since 2021-12-1
+ * @since 2021-12-01
  */
 public class Md5SignHandler implements ISignHandler {
 
